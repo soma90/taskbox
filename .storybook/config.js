3227d4ba746f5eb7effd60197cc979a16fc70121
kyutae <soma90-1@hanmail.net>
2019-11-12 17:51:02 +0900

    data section

---
+++ .storybook/config.js
@@ -0,0 +1,20 @@
+/*
+import { configure } from '@storybook/react';
+
+// automatically import all files ending in *.stories.js
+configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+*/
+
+
+import { configure } from '@storybook/react';
+import requireContext from 'require-context.macro';
+
+import '../src/index.css';
+
+const req = requireContext('../src/components', true, /\.stories\.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
